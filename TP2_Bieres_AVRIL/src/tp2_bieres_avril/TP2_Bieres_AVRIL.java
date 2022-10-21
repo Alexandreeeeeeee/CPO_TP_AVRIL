2fca2ce0d17b65b9ea65342f3e8698da05773f74
Alexandre Avril <alexandre.avril@epfedu.fr>
2022-10-21 16:54:05 +0200

    Partie 1, question 9

--- TP2_Bieres_AVRIL/src/tp2_bieres_avril/TP2_Bieres_AVRIL.java
+++ TP2_Bieres_AVRIL/src/tp2_bieres_avril/TP2_Bieres_AVRIL.java
@@ -9,12 +9,22 @@ package tp2_bieres_avril;
  * @author alexa
  */
 public class TP2_Bieres_AVRIL {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        
+        BouteilleBiere uneBiere = new BouteilleBiere() ;
+        uneBiere.Nom = "Cuvée des trolls";
+        uneBiere.degresAlcool = 7.0 ;
+        uneBiere.brasserie = "Dubuisson" ; 
+        uneBiere.ouverte = false ; 
+        uneBiere.lireEtiquette() ; 
+        
+        BouteilleBiere Biere_2 = new BouteilleBiere() ;
+        Biere_2.Nom = "Leffe";
+        Biere_2.degresAlcool = 6.0 ;
+        Biere_2.brasserie = "Abbaye de Leffe" ; 
+        Biere_2.lireEtiquette() ;
 }
