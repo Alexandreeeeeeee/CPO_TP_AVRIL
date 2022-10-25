294ddc9da9c83a2924f7671bff680ab047c8d52e
Alexandre Avril <alexandre.avril@epfedu.fr>
2022-10-25 10:16:34 +0200

    TP1, partie 2 convertisseur de température : demande à l'utilisateur de saisir une température à convertir

---
+++ TP1_CONVERTISSEUR_AVRIL/src/tp1_convertisseur_avril/TP1_CONVERTISSEUR_AVRIL.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_convertisseur_avril;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alexa
+ */
+public class TP1_CONVERTISSEUR_AVRIL {
+
+    static Scanner sc = new Scanner(System.in); // déclaration du scanner.
+    
+    public static void main(String[] args) {
+        System.out.println("\nEntrer une températeur à convertir : "); // demande à l'utilisateur.
+        Double Temp = sc.nextDouble(); // stock le choix de l'utilisateur.
+    }
+    
+}
