88145c20e06f36ce100839dc748f055f02e7eb67
Alexandre Avril <alexandre.avril@epfedu.fr>
2022-10-25 10:33:26 +0200

    Première méthode crée pour convertir des celcius en kelvin

---
+++ TP1_CONVERTISSEUR_AVRIL/src/tp1_convertisseur_avril/Convertisseur.java
@@ -0,0 +1,19 @@
+package tp1_convertisseur_avril;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public class Convertisseur {
+    // On définit les attributs de la classe BouteilleBiere.
+    Double nbConversions ; 
+    
+public void Convertisseur () {
+    Convertisseur c = new Convertisseur () ; 
+}
+public void CelciusVersKelvin (){
+        double kelvin 
+        Kelvin = temp + 273.15 ;
+        return Kelvin ;
+}}
