b3fe542a90775076aa631169b771b475520e4dc7
Alexandre Avril <alexandre.avril@epfedu.fr>
2022-11-08 16:33:01 +0100

    Création de 2 épées et 2 batons + Affichage dans le console.

--- TP3_Heritage/src/tp3_heritage/TP3_Heritage.java
+++ TP3_Heritage/src/tp3_heritage/TP3_Heritage.java
@@ -13,8 +13,16 @@ public class TP3_Heritage {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Arme 
-    }
-    
+    public static void main(String[] args) {        
+        Epée arme1 = new Epée ("Excalibur", 7,5); // déclaration des 2 épées et batons de la classe Arme.
+        Epée arme2 = new Epée ("Durandal", 4,7);
+        Baton arme3 = new Baton ("Chêne",4,5);
+        Baton arme4 = new Baton ("Charme", 5,6);
+        
+        System.out.println(arme1); // affichage dans la console
+        System.out.println(arme2);
+        System.out.println(arme3);
+        System.out.println(arme4);
+
+    }   
 }
