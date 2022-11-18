7b67f3a5bfa9859928dd02ea1a3a5750bd058b34
Alexandre Avril <alexandre.avril@epfedu.fr>
2022-11-18 11:10:14 +0100

    classe Jeton créé et commentée + TEST d'affichage dans la console

---
+++ SP4_console_AVRIL_Alexandre/src/sp4_console_avril_alexandre/SP4_console_AVRIL_Alexandre.java
@@ -0,0 +1,12 @@
+package sp4_console_avril_alexandre;
+
+public class SP4_console_AVRIL_Alexandre {
+
+    public static void main(String[] args) {
+        
+        Jeton test = new Jeton ("Jaune"); // Test de la classe Jeton.
+        System.out.println(test); // bon affichage de la couleur J ou R du jeton dans la console.
+        
+    }
+    
+}
