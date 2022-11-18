ce080e34471d8da9b78e561f813178682856f9ad
Alexandre Avril <alexandre.avril@epfedu.fr>
2022-11-18 10:57:08 +0100

    classe Jeton créé et commentée.

---
+++ SP4_console_AVRIL_Alexandre/src/Jeton.java
@@ -0,0 +1,16 @@
+public class Jeton {
+    private String couleur ; //déclaration attribut couleur en privé.
+    
+    public Jeton(String unecouleur){
+        couleur = unecouleur; // affectation de la couleur au jeton.
+    }
+    public String lireCouleur(){
+        return couleur; // on renvoie la couleur du jeton.
+    }
+}
+
+    
+
+
+
+
