acf1ce36c6ffb2be7cc21234bb040336dd00f62b
Alexandre Avril <alexandre.avril@epfedu.fr>
2022-11-18 11:47:06 +0100

    classe Jeton créé et commentée + TEST d'affichage dans la console

---
+++ SP4_console_AVRIL_Alexandre/src/sp4_console_avril_alexandre/CelluleDeGrille.java
@@ -0,0 +1,45 @@
+package sp4_console_avril_alexandre;
+
+public class CelluleDeGrille {
+    private Jeton jetonCourant;
+    private boolean avoirTrouNoir;
+    boolean avoirDesintegrateur;
+    
+    public CelluleDeGrille () {
+        jetonCourant = null;
+        avoirTrouNoir = false;
+        avoirDesintegrateur = false;
+    }
+    
+    public boolean presenceJeton(Jeton Jeton) {
+        if (jetonCourant == null) {
+            jetonCourant = Jeton;
+            return false;
+        }
+        else {
+            System.out.print("La cellule est pleine !");
+            return true;
+        }
+    }
+    public String lireCouleurDuJeton() { // lecteur d'une case, elle indique la couleur du jeton, ou rien.
+        if (jetonCourant == null) {
+            return "vide";
+        } 
+        else {
+            return jetonCourant.lireCouleur();
+        }
+    }
+    public boolean placerTrouNoir(){
+        if (avoirTrouNoir == false){
+            avoirTrouNoir = true;
+            System.out.print("Un trou noir a été ajouté !");
+        }
+    }
+    
+}
+        
+    
+    
+        
+    
+
